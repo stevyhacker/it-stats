522a379b90573e8440d7f7bcf21cb61d83a1d90c
Stevan Bogosavljevic <stevyhacker@gmail.com>
2020-09-12 23:43:23 +0200

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import YearList from "./YearList/YearList";
-import ChartList from "./ChartList/ChartList";
 
 function App() {
     return (
         <div className="app">
-            <YearList/>
+
+            <section>
+                <YearList/>
+            </section>
+            <footer className="footer text-white-50" >
+                <p className="footer-link">Made by <a href={"https://github.com/stevyhacker"}>Stevan Bogosavljević</a></p>
+                <p className="footer-link">Project and the dataset is open source <a href={"https://github.com/stevyhacker/it-stats"}>@GitHub</a></p>
+                <p >Suggestions and improvements are welcome in the form of pull requests :)</p>
+            </footer>
+
         </div>
     );
 }
