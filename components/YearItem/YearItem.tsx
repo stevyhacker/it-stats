2a88b7951bd0bace72972ac2f3129dbedf17cea5
chetan chudasama <chetanit2013@gmail.com>
2024-05-01 16:50:44 +0530

    update font and fix label issue

--- components/YearItem/YearItem.tsx
+++ components/YearItem/YearItem.tsx
@@ -1,75 +1,84 @@
-import React from 'react';
-import BootstrapTable from 'react-bootstrap-table-next';
-import ToolkitProvider, {Search} from 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit';
-import './YearItem.css'
+"use client";
+import React from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import ToolkitProvider, {
+    Search,
+} from "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit";
+import "./YearItem.css";
 import ChartTopFiveByEmployees from "../ChartTopFive/ChartTopFiveByEmployees";
 
-import './ResponsiveTable.css'
+import "./ResponsiveTable.css";
 import HeroNumbers from "../HeroNumbers/HeroNumbers";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
+import { Chart, registerables } from "chart.js";
 
-const {SearchBar} = Search;
+const { SearchBar } = Search;
 
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'decimal',
-    currency: 'EUR',
+Chart.register(...registerables);
+
+Chart.defaults.color = "#fff";
+Chart.defaults.font.size = 14;
+
+const formatter = new Intl.NumberFormat("en-US", {
+    style: "decimal",
+    currency: "EUR",
 });
 
 function currencyFormatter(cell, row) {
-    return (
-        <span className={"money-label"}>{formatter.format(cell)} €</span>
-    );
+    return <span className={"money-label"}>{formatter.format(cell)} €</span>;
 }
 
 const columnStyle = {
-    fontSize: '1.1rem',
-    textAlign: 'right'
-}
+    fontSize: "1.1rem",
+    textAlign: "right",
+};
 
 const headerStyle1 = {
-    color: '#c4c0c0',
-    fontSize: '1rem',
-    backgroundColor: '#48316e',
+    color: "#c4c0c0",
+    fontSize: "1rem",
+    backgroundColor: "#48316e",
     whiteSpace: "nowrap",
-    width: '12rem',
-    textAlign: 'center'
-}
+    width: "12rem",
+    textAlign: "center",
+};
 
 const headerStyle2 = {
-    color: '#c4c0c0',
-    fontSize: '1rem',
-    backgroundColor: '#422966',
+    color: "#c4c0c0",
+    fontSize: "1rem",
+    backgroundColor: "#422966",
     whiteSpace: "nowrap",
-    width: '12rem',
-    textAlign: 'center'
-}
+    width: "12rem",
+    textAlign: "center",
+};
 
 const headerStyle3 = {
-    color: '#c4c0c0',
-    fontSize: '1rem',
-    backgroundColor: '#48316e',
+    color: "#c4c0c0",
+    fontSize: "1rem",
+    backgroundColor: "#48316e",
     whiteSpace: "nowrap",
-    width: '10rem',
-    textAlign: 'center'
-}
+    width: "10rem",
+    textAlign: "center",
+};
 
 const headerStyle4 = {
-    color: '#c4c0c0',
-    fontSize: '0.85rem',
-    backgroundColor: '#422966',
+    color: "#c4c0c0",
+    fontSize: "0.85rem",
+    backgroundColor: "#422966",
     whiteSpace: "nowrap",
-    width: '12rem',
-    textAlign: 'center'
-}
+    width: "12rem",
+    textAlign: "center",
+};
 
 const columns = [
     {
-        text: "Name", dataField: "name", sort: true,
+        text: "Name",
+        dataField: "name",
+        sort: true,
         headerStyle: headerStyle2,
         style: {
-            fontWeight: 'bold',
-            fontSize: '1.25rem',
-        }
+            fontWeight: "bold",
+            fontSize: "1.25rem",
+        },
     },
     {
         text: "Total Revenue",
@@ -77,71 +86,81 @@ const columns = [
         formatter: currencyFormatter,
         sort: true,
         headerStyle: headerStyle1,
-        style: columnStyle
-    }, {
+        style: columnStyle,
+    },
+    {
         text: "Profit",
         dataField: "profit",
-        formatter: currencyFormatter, sort: true,
+        formatter: currencyFormatter,
+        sort: true,
         headerStyle: headerStyle2,
-        style: columnStyle
-    }, {
-        text: "Employees", dataField: "employeeCount", sort: true,
+        style: columnStyle,
+    },
+    {
+        text: "Employees",
+        dataField: "employeeCount",
+        sort: true,
         headerStyle: headerStyle3,
-        style: columnStyle
-    }, {
+        style: columnStyle,
+    },
+    {
         text: "Average Pay*",
         dataField: "averagePay",
         formatter: currencyFormatter,
         sort: true,
         sortFunc: (a, b, order, dataField, rowA, rowB) => {
-            if (order === 'asc') {
+            if (order === "asc") {
                 return rowA.averagePay - rowB.averagePay;
             }
             return rowB.averagePay - rowA.averagePay;
         },
         headerStyle: headerStyle2,
-        align: 'right',
-        style: {columnStyle}
-    }, {
+        align: "right",
+        style: { columnStyle },
+    },
+    {
         text: "Net wage costs",
         dataField: "netPayCosts",
-        formatter: currencyFormatter, sort: true,
+        formatter: currencyFormatter,
+        sort: true,
         headerStyle: headerStyle1,
-        align: 'right',
-        style: {columnStyle}
+        align: "right",
+        style: { columnStyle },
     },
     {
         text: "Revenue per employee",
         dataField: "incomePerEmployee",
-        formatter: currencyFormatter, sort: true,
-        align: 'right',
+        formatter: currencyFormatter,
+        sort: true,
+        align: "right",
         headerStyle: headerStyle4,
-        style: {columnStyle}
-    }
-]
+        style: { columnStyle },
+    },
+];
 
-const defaultSorting = [{
-    dataField: 'totalIncome',
-    order: 'desc'
-}];
-
+const defaultSorting = [
+    {
+        dataField: "totalIncome",
+        order: "desc",
+    },
+];
 
 function YearItem(props) {
 
     // let initiallyOpened = false
     if (Number.parseInt(props.item.year) < 2020) {
         // @ts-ignore
-        columns[4].hidden = true
+        columns[4].hidden = true;
         // @ts-ignore
-        columns[5].hidden = true
+        columns[5].hidden = true;
     } else {
         // @ts-ignore
-        columns[4].hidden = false
+        columns[4].hidden = false;
         // @ts-ignore
-        columns[5].hidden = false
+        columns[5].hidden = false;
     }
 
-    const router = useRouter()
+    const router = useRouter();
 
     const rowEvents = {
         onClick: (e, row, rowIndex) => {
@@ -150,49 +169,55 @@ function YearItem(props) {
         },
         onMouseEnter: (e, row, rowIndex) => {
             // console.log(`enter on row with index: ${rowIndex}`);
-        }
+        },
     };
 
-    // const data = require('../assets/stats.json');
-    // data.forEach(year => {
-    //     year.companyList.forEach(company => {
-    //         company.incomePerEmployee = company.totalIncome / company.employeeCount;
+    // const data = require("../assets/stats.json");
+    // data.forEach((year) => {
+    //     year.companyList.forEach((company) => {
+    //         company.incomePerEmployee =
+    //             company.totalIncome / company.employeeCount;
     //         company.incomePerEmployee = Math.round(company.incomePerEmployee);
     //     });
     // });
     // console.log(data);
 
     return (
-
         <div>
-
-            <HeroNumbers item={props.item}/>
+            <HeroNumbers item={props.item} />
 
             <ToolkitProvider
                 keyField="id"
                 data={props.item.companyList}
                 columns={columns}
                 condensed={true}
-                search>
-                {props => (
+                search
+            >
+                {(props) => (
                     <div>
                         <div className={"searchBar"}>
-                            <SearchBar {...props.searchProps} placeholder="Search by name" />
+                            <SearchBar
+                                {...props.searchProps}
+                                placeholder="Search by name"
+                            />
                         </div>
-                        <p className="text-center table-label ">Companies sorted by Total Revenue</p>
+                        <p className="text-center table-label ">
+                            Companies sorted by Total Revenue
+                        </p>
                         <BootstrapTable
                             defaultSorted={defaultSorting}
                             wrapperClasses="table-responsive"
                             rowEvents={rowEvents}
                             hover={true}
-                            rowClasses={"text-white companyItem"} {...props.baseProps}/>
+                            rowClasses={"text-white yearItem"}
+                            {...props.baseProps}
+                        />
                     </div>
                 )}
             </ToolkitProvider>
-            <ChartTopFiveByEmployees companyList={props.item.companyList}/>
-
+            <ChartTopFiveByEmployees companyList={props.item.companyList} />
         </div>
     );
 }
 
-export default YearItem
+export default YearItem;
--- components/YearList/YearList.tsx
+++ components/YearList/YearList.tsx
@@ -1,30 +1,38 @@
 "use client";
 
-import React, {useState} from 'react';
-import './YearList.css'
+import React, { useState } from "react";
+import "./YearList.css";
 import YearItem from "../YearItem/YearItem";
 import statsData from "@/assets/stats.json";
 
-function YearList() {
-
-    const [selectedYear, setSelectedYear] = useState<string>('2023');
-
+const YearList = () => {
+    const [selectedYear, setSelectedYear] = useState<string>("2023");
 
     function selectYear(e: React.MouseEvent<HTMLButtonElement>) {
         console.log(e.currentTarget.textContent);
         setSelectedYear(e.currentTarget.textContent);
     }
 
-    return <div className={"overflow-auto"}>
+    return (
+        <div className={"overflow-auto"}>
+            <div className={"btn-group mb-4"}>
+                {statsData.map((item) => (
+                    <button
+                        className={"btn text-white border"}
+                        key={item.year}
+                        onClick={selectYear}
+                    >
+                        {item.year}
+                    </button>
+                ))}
+            </div>
 
-        <div className={"btn-group mb-4"}>
-            {statsData.map(item => (
-                <button className={"btn text-white border"} key={item.year} onClick={selectYear}>{item.year}</button>
-            ))}
+            <YearItem
+                key={selectedYear}
+                item={statsData.find((item) => item.year === selectedYear)}
+            />
         </div>
+    );
+};
 
-        <YearItem key={selectedYear} item={statsData.find(item => item.year === selectedYear)}/>
-    </div>
-}
-
-export default YearList
+export default YearList;
